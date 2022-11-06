2becc6bb0bd24ef13d5be0da320f4bec76b025f0
AMEXif <amex.if.email@seznam.cz>
2022-11-06 21:50:37 +0100

    Read input file

--- src/Ayanami.c
+++ src/Ayanami.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+long fileSize(FILE* file) {
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    return size;
+}
+
 char* fileContents(char* path) {
     FILE* file = fopen(path, "r");
+    if (!file) {
+        printf("Error while opening %s\n", path);
+        return NULL;
+    }
+    long size = fileSize(file);
+    char* contents = malloc(size + 1);
+    size_t bytesRead = fread(contents, 1, size, file);
+    if (bytesRead != size) {
+        free(contents);
+        return NULL;
+    }
+    // Make sure to null-terminate the file contents so that we don't get any string-related issues.
+    contents[size] = '\0';
+    return contents;
 }
 
 void printUsage(char** argv) {
@@ -14,5 +35,8 @@ int main(int argc, char** argv) {
         printUsage(argv);
         exit(0);
     }
-    printf("Rei Says hi!\n");
+
+    char* filepath = argv[1];
+    char* contents = fileContents(filepath);
+    printf("%s\n", contents);
 }
